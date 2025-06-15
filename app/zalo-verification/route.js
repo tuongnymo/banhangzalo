5f4a03a446800ed2566176abd13a071297c6d8b9
tuongnymo <tuongnymo@gmail.com>
2025-06-15 17:33:50 +0700

    Thêm xác thực Zalo

---
+++ app/zalo-verification/route.js
@@ -0,0 +1,7 @@
+export async function GET() {
+  return new Response('zalo-platform-site-verification=VkIk3O_2DZ9Mr-GHqUH-53NlyYdPcIHpC38u', {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
