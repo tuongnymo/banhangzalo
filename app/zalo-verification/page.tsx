f967c1ec7d93f2c9cff957157fde4230a494f21a
tuongnymo <tuongnymo@gmail.com>
2025-06-15 17:37:08 +0700

    Thêm xác th?c Zalo

---
+++ app/zalo-verification/page.tsx
@@ -0,0 +1,9 @@
+const ZaloVerification = () => {
+  return (
+    <pre>
+      zalo-platform-site-verification=VkIk3O_2DZ9Mr-GHqUH-53NlyYdPcIHpC38u
+    </pre>
+  );
+};
+
+export default ZaloVerification;
