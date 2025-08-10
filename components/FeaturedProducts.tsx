5e87ae8d02f3f75e16b9098060c48a0ed01c213c
tuongnymo <tuongnymo@gmail.com>
2025-08-10 16:30:39 +0700

    cap nhat anh

--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -82,7 +82,7 @@ function FeaturedProductCard({ product }: { product: any }) {
   >
     <Link href={`/product/${product.id}`}>
       {/* Khung ảnh */}
-      <div className="relative w-full pb-[100%] overflow-hidden bg-gray-100">
+      <div className="relative h-64 w-full overflow-hidden bg-gray-100">
         <Image
           src={product.images?.[0] || "/placeholder.svg"}
           alt={product.name}
