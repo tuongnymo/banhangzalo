7d5718a18ffafb068f303a1fbd3da92b8f8c3313
tuongnymo <tuongnymo@gmail.com>
2025-08-11 11:42:34 +0700

    cập nhật sản phẩm bán chạy

--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -39,7 +39,7 @@ export default function FeaturedProducts() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-5">
          {products.map((product) => (
   <Link key={product.id} href={`/product/${product.id}`}>
     <ClickableProductCard
