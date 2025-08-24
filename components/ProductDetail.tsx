74600c03d84c1ca6b65f19368fb4ef14dee9e3dd
tuongnymo <tuongnymo@gmail.com>
2025-08-24 09:59:49 +0700

    L?n d?u d?y code

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -269,7 +269,7 @@ export default function ProductDetail({ product }: ProductDetailProps) {
                   d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
                 />
               </svg>
-              <span className="ml-2">Đổi trả trong vòng 30 ngày</span>
+              <span className="ml-2">Đổi trả trong vòng 7 ngày nếu lỗi từ nhà sản xuất</span>
             </div>
           </div>
         </div>
